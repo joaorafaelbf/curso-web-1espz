b63977d60c63def6cea45c8b165a793e14ddd3ab
joaorafaelbf <you@example.com>
2026-03-17 21:03:59 -0300

    O

---
+++ script.js
@@ -0,0 +1,65 @@
+// console.log("oi")
+
+// console.info("o")
+
+// console.warn("aviso")
+
+// console.error("errou ai")
+
+// -----------------------------------
+
+// console.table([
+    // {oi: 1, tarefa: "EstudarJS"},
+    // {oi:2, tarefa: "Praticar código"},
+    // {oi: 3, tarefa: "Comer"}])
+
+    // console.group("Grupo de logs")
+    // console.log("log 1")
+    // console.log("log 2")
+    // console.log("log 3")
+    // console.groupEnd()
+
+/*let casa = 
+    console.time("timer")
+    if (casa = "casa") 
+        {console.log("casa")}
+    console.timeEnd("timer")*/
+
+    // let texto = "oi"
+    // console.log
+    // ("nome = " + texto + " " + "tipo:" + " " + typeof(texto))
+
+
+    // let number = 1
+    // console.log
+    // ("number = " + number + " " + "type:" + " " + typeof(number))
+
+    //  let isCompleted = true
+    // console.log
+    // ("isCompleted = " + isCompleted + " " 
+    //     + "isCompleted:" + " " + typeof(isCompleted))
+
+    // let semValor
+    // console.log
+    // ("semValor = " + semValor + " " + "tipo:" + " " + typeof(semValor))
+
+    // let uniqueId = Symbol("id")
+
+    // let bigNumero = 99999999999n
+    // console.log
+    // ("bigNumero = " + bigNumero
+    //      + " " + "tipo:" + " " + typeof(bigNumero))
+
+
+// let task = [{id: 1,
+//      title: "Aprender Javascript",
+//       completed: false,
+//        assignee: {name: "João", email: "alalgmail.com" }}]
+
+//        console.table(task)
+
+let hoje = new Date()
+console.log(hoje)
+
+let amanhã = new Date("03-18-2026")
+console.log(amanhã)
